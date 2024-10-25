f8bd6be94ddd7f26639fd8e5362f3410a4c1dd8a
Andrewdragom <andreasdragomirov@yandex.com>
2024-10-26 01:45:00 +0500

    Add files via upload

---
+++ 02.sql
@@ -0,0 +1,2 @@
+-- Create your SELECT statement here
+SELECT id, name, age FROM people WHERE age > 50 ORDER BY age DESC
---
+++ 05.sql
@@ -0,0 +1,2 @@
+/*  SQL  */
+SELECT FLOOR(number1) as number1, CEIL(number2) as number2 FROM decimals 
